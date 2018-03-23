9b0651133143f5d51144dbba9f8cc297a812ef3c
Ali <mailalisabet@gmail.com>
2018-03-23 05:48:50 +0430

    Initial commit

---
+++ app/src/main/java/com/alisabet/location/MapsActivity.kt
@@ -0,0 +1,39 @@
+package com.alisabet.location
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+    //1.make Google map activity instead of Blank activity at first
+    //2.copy https://console.developers.google.com/flows/enableapi?apiid=map... from google_maps_api.xml in browser
+    //3.follow instructions in browser, get API key, and write instead of YOUR_API_KEY in google_maps_api.xml file
+    //4.get permissions
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
